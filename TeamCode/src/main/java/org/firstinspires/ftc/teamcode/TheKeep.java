58e5b023032d1b9cda1eb26ef866b199c29caad4
MrChips10 <jason.fogiel@outlook.com>
2025-11-08 12:56:44 -0500

    Update to The Keep
    Updated the program to not use a continues rotation servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TheKeep.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TheKeep.java
@@ -12,14 +12,15 @@ public class TheKeep extends OpMode {
     private DcMotor rightDrive = null;
     private DcMotor shooter = null;
     private Servo ballEjector = null;
-    private CRServo spinIndexer = null;
+    private Servo spinIndexer = null;
+    private double spinPosition = 0.6;
     @Override
     public void init() {
 
         leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
         rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
         ballEjector = hardwareMap.get(Servo.class, "ballEjector");
-        spinIndexer = hardwareMap.get(CRServo.class, "spinIndexer");
+        spinIndexer = hardwareMap.get(Servo.class, "spinIndexer");
         shooter = hardwareMap.get(DcMotor.class, "shooter");
         //These lines setup the motors and servos
 
@@ -36,15 +37,23 @@ public class TheKeep extends OpMode {
         rightDrive.setPower((gamepad1.left_stick_y+gamepad1.left_stick_x)/2);
         // These lines control the wheel motors
 
-        ballEjector.setPosition(gamepad1.right_stick_y);
-        // This controls the ball ejector
+        if (gamepad1.rightBumperWasPressed())  spinPosition = spinPosition + 0.2;
+        if (gamepad1.leftBumperWasPressed()) spinPosition = spinPosition - 0.2;
+        if (spinPosition>1) spinPosition = 1;
+        if (spinPosition<0) spinPosition = 0;
+        spinIndexer.setPosition(spinPosition);
+        // These lines control the spin indexer
 
-        spinIndexer.setPower(gamepad1.left_trigger-gamepad1.right_trigger); //
-        // This controls the fidget tech
+        if (gamepad1.right_stick_y>0) ballEjector.setPosition(.2);
+        if (gamepad1.right_stick_y<=0) ballEjector.setPosition(0);
+
+        // This controls the ball ejector
 
         if (gamepad1.dpad_up) shooter.setPower(1);
         if (gamepad1.dpad_down) shooter.setPower(0);
         // These lines control the shooting flywheel
+        telemetry.addData("SpinIndexer Position", spinPosition);
+        telemetry.update();
 
     } // This section controls all the motors using the remote control
 
