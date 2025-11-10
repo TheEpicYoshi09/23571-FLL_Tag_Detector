24e4b836a82b285c95c35ebe7888ba11b74af6a8
naushadwaqar <nwaqar970209@gmail.com>
2025-11-10 18:21:35 -0500

    fixed the helper cals thing and added teleop logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/SpindexerTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/SpindexerTester.java
@@ -6,110 +6,88 @@ import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 
-import org.firstinspires.ftc.teamcode.subsystems.Actuator;
 import org.firstinspires.ftc.teamcode.subsystems.Indexer;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
 
 @TeleOp(name = "SpindexerTest", group = "Teleop")
 public class SpindexerTester extends LinearOpMode {
-    ColorSensor colorSensor;
-    Actuator actuator;
+
     Indexer indexer;
     Intake intake;
     GamepadEx gp2;
 
     @Override
-    // Make sure to check if busy every time you do an indexer move
-    public void runOpMode(){
-        colorSensor = hardwareMap.get(ColorSensor.class, "color");
+    public void runOpMode() {
         indexer = new Indexer(hardwareMap);
-        actuator = new Actuator(hardwareMap);
         intake = new Intake(hardwareMap);
         gp2 = new GamepadEx(gamepad2);
 
         waitForStart();
+
         indexer.startIntake();
-        while(opModeIsActive()){
+
+        while (opModeIsActive()) {
             gp2.readButtons();
+
             telemetry.addData("CurrentState: ", indexer.getState());
             telemetry.addData("NextState: ", indexer.nextState());
 
-            if(gp2.wasJustPressed(GamepadKeys.Button.DPAD_UP)){
+            if (gp2.wasJustPressed(GamepadKeys.Button.DPAD_UP)) {
                 if (!indexer.isBusy()) {
                     indexer.moveTo(indexer.nextState());
                 }
             }
-            if(gp2.wasJustPressed(GamepadKeys.Button.A))
-            {
+
+            if (gp2.wasJustPressed(GamepadKeys.Button.A)) {
                 if (!indexer.isBusy()) {
                     indexer.setIntaking(true);
                 }
             }
-            if(gp2.wasJustPressed(GamepadKeys.Button.B))
-            {
+
+            if (gp2.wasJustPressed(GamepadKeys.Button.B)) {
                 if (!indexer.isBusy()) {
                     indexer.setIntaking(false);
                 }
             }
-            if(gp2.getButton(GamepadKeys.Button.X)) {
-                actuator.set(true);
-            }
-            else if(gp2.getButton(GamepadKeys.Button.Y)) {
-                actuator.set(false);
-            }
 
-            if(gp2.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER)>0.01)
-            {
+            if (gp2.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) > 0.01) {
                 intake.run(true);
-            }
-            else {
+            } else {
                 intake.run(false);
             }
 
-            telemetry.addData("Light Detected:",((OpticalDistanceSensor) colorSensor).getLightDetected());
-            telemetry.addData("Red", colorSensor.red());
-            telemetry.addData("Green", colorSensor.green());
-            telemetry.addData("Blue", colorSensor.blue());
+            // Call update each loop to continuously control the servo position
+            indexer.update();
 
-            int r = colorSensor.red();
-            int g = colorSensor.green();
-            int b = colorSensor.blue();
+            // Update color scanning timing and sensor reading
+            //indexer.updateColorScanning();
 
-            String mainColor = getMainColor(r, g, b);
-            telemetry.addData("Color Detected overall:",mainColor);
-
-            indexer.updateColorScanning();
             telemetry.update();
         }
     }
 
     @NonNull
     private static String getMainColor(int r, int g, int b) {
-        double total = r+g+b;
+        double total = r + g + b;
         double rRatio = r / total;
         double gRatio = g / total;
         double bRatio = b / total;
 
-        String mainColor;
-
         if (rRatio > 0.45 && gRatio < 0.35 && bRatio < 0.35) {
-            mainColor = "Red";
+            return "Red";
         } else if (rRatio > 0.45 && gRatio > 0.25 && bRatio < 0.20) {
-            mainColor = "Orange";
+            return "Orange";
         } else if (rRatio > 0.38 && gRatio > 0.38 && bRatio < 0.25) {
-            mainColor = "Yellow";
+            return "Yellow";
         } else if (gRatio > 0.45 && rRatio < 0.35 && bRatio < 0.35) {
-            mainColor = "Green";
+            return "Green";
         } else if (bRatio > 0.45 && rRatio < 0.35 && gRatio < 0.35) {
-            mainColor = "Blue";
+            return "Blue";
         } else if (rRatio > 0.35 && bRatio > 0.35 && gRatio < 0.30) {
-            mainColor = "Purple";
+            return "Purple";
         } else {
-            mainColor = "Unclear";
+            return "Unclear";
         }
-        return mainColor;
     }
 }
