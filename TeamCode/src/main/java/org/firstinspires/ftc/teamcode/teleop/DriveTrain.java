ce9e288e58bfef7826aaf3d08ef30e951753dc9e
elizabethchung <elizabethychung@gmail.com>
2025-11-16 14:45:31 -0500

    added files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DriveTrain.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
