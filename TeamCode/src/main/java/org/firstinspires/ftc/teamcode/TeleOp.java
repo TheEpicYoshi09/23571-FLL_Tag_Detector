209af8ccc754a5e8129b1c67e87770592db9f094
Rowan Shenoy <rowans@hotmail.com>
2025-11-22 11:56:39 -0500

    Ready for testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -0,0 +1,85 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class TeleOp extends LinearOpMode {
+    DcMotor frontLeft, frontRight, backLeft, backRight;
+
+    Servo gate;
+    DcMotorEx launcher;
+    DcMotor intake;
+
+    boolean intakeAction = false;
+
+    public void runOpMode() {
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        gate = hardwareMap.get(Servo.class, "gate");
+        launcher = hardwareMap.get(DcMotorEx.class, "launcher");
+        intake = hardwareMap.get(DcMotor.class, "intake");
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        launcher.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        launcher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double drive = -gamepad1.left_stick_y;
+            double strafe = gamepad1.left_stick_x;
+            double rotate = gamepad1.right_stick_x;
+
+            double frontLeftPower = drive + strafe + rotate;
+            double frontRightPower = drive - strafe - rotate;
+            double backLeftPower = drive - strafe + rotate;
+            double backRightPower = drive + strafe - rotate;
+
+            frontLeft.setPower(frontLeftPower);
+            frontRight.setPower(frontRightPower);
+            backLeft.setPower(backLeftPower);
+            backRight.setPower(backRightPower);
+
+            if (gamepad2.a) {
+                gate.setPosition(1.0);
+            } else if (gamepad2.b) {
+                gate.setPosition(0.0);
+            }
+
+            if (gamepad2.right_trigger > 0.1) {
+                launcher.setVelocity(6000 * gamepad2.right_trigger);
+            } else {
+                launcher.setVelocity(0);
+            }
+
+            if (gamepad2.dpad_down) {
+                intakeAction=true;
+            }
+
+            if (intakeAction) {
+                intake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                intake.setTargetPosition(500);
+                intake.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                intake.setPower(0.25);
+                if (!intake.isBusy()) {
+                    intakeAction=false;
+                }
+            } else {
+                if (gamepad2.left_trigger > 0.1) {
+                    intake.setPower(1.0);
+                } else if (gamepad2.left_bumper) {
+                    intake.setPower(-1.0);
+                } else {
+                    intake.setPower(0.0);
+                }
+            }
+        }
+    }
+}
