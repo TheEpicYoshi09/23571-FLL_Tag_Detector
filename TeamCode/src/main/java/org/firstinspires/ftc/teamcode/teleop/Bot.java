e890fd66f5d26e4663b511e76a1e15e548ce6fa9
arnavjosh <78702246+arnavjosh@users.noreply.github.com>
2025-11-24 19:53:25 -0500

    Misc stuff (Comments, telemetry, unnecessary code, more FSM)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Bot.java
@@ -21,19 +21,11 @@ public class Bot {
     private final Outtake outtake;
     private final Movement movement;
 
-    //private final AprilTag aprilTag;
-    //private final AprilTagAimer aprilAimer;
     private final GamepadEx g1;
     private final GamepadEx g2;
     private final Telemetry telemetry;
 
-    //private long lastAimUpdate = 0;
-    //private double lastTurnCorrection = 0;
-
-    //private boolean continuousAprilTagLock = false;
     private boolean fieldCentric = false;
-
-    //private static final long AIM_UPDATE_INTERVAL_MS = 50;
 
     public enum FSM{
         SortOuttake,
@@ -41,19 +33,19 @@ public class Bot {
         Intake,
         Endgame
     }
+
+    public FSM state;
+
     public Bot(HardwareMap hardwareMap, Telemetry tele, Gamepad gamepad1, Gamepad gamepad2) {
         intake = new Intake(hardwareMap);
         indexer = new Indexer(hardwareMap);
         actuator = new Actuator(hardwareMap);
         outtake = new Outtake(hardwareMap);
         movement = new Movement(hardwareMap);
-
-        //aprilTag = new AprilTag(hardwareMap);
-        //aprilAimer = new AprilTagAimer(hardwareMap);
-
         g1 = new GamepadEx(gamepad1);
         g2 = new GamepadEx(gamepad2);
         telemetry = tele;
+        state = FSM.Intake;
     }
     public void teleopInit() {
         indexer.startIntake();
@@ -61,32 +53,6 @@ public class Bot {
     public void teleopTick() {
         g1.readButtons();
         g2.readButtons();
-
-        /*
-        //apriltag turn correction
-        double turnCorrection = 0;
-
-        if (continuousAprilTagLock) {
-            long now = System.currentTimeMillis();
-
-            if (now - lastAimUpdate >= AIM_UPDATE_INTERVAL_MS) {
-                lastAimUpdate = now;
-
-                aprilTag.scanGoalTag();
-                double bearing = aprilTag.getBearing();
-
-                if (!Double.isNaN(bearing)) {
-                    lastTurnCorrection = aprilAimer.calculateTurnPowerToBearing(bearing);
-                } else {
-                    lastTurnCorrection = 0;
-                }
-            }
-
-            turnCorrection = 0.9 * lastTurnCorrection;  // smooth decay
-        }
-        */
-
-        //drivetrain control
         if (fieldCentric) {
             movement.teleopTickFieldCentric(
                     g1.getLeftX(),
@@ -103,23 +69,22 @@ public class Bot {
                     0
             );
         }
-
-        // Toggle field centric
         if (g1.getButton(GamepadKeys.Button.LEFT_STICK_BUTTON)) fieldCentric = !fieldCentric;
-
-
 
         // intake control
         if (indexer.getIntaking()) {
-            if(g2.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER)>0.01){
+            if(g2.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER)>0.01)
+            {
                 intake.run();
             }
-            else {
+            else
+            {
                 intake.stop();
             }
         }
 
         //outtake control
+        //should be replaced with better automation i'll do that eventually
         if (!indexer.getIntaking()) {
             if (g2.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) > 0.01) {
                 outtake.run();
@@ -128,13 +93,7 @@ public class Bot {
             }
         }
 
-        // spindexer control
-        // Advance state
-        if (g2.wasJustPressed(GamepadKeys.Button.DPAD_RIGHT)) {
-            if (!indexer.isBusy()) indexer.moveTo(indexer.nextState());
-        }
-
-        //actuator control
+        //should be replaced with better automation i'll do that eventually
         if (!indexer.getIntaking()) {
             if (g2.wasJustPressed(GamepadKeys.Button.DPAD_UP)) {
                 actuator.up();
@@ -143,48 +102,12 @@ public class Bot {
                 actuator.down();
             }
         }
-
-        // Scan obelisk
-        /*
-        if (g2.wasJustPressed(GamepadKeys.Button.DPAD_LEFT)) {
-            aprilTag.scanObeliskTag();
-            telemetry.addData("Obelisk ID", aprilTag.getObeliskId());
-        }
-
-        // Set intaking ON
-        if (g2.wasJustPressed(GamepadKeys.Button.A) && !actuator.isActivated()) {
-            if (!indexer.isBusy()) indexer.setIntaking(true);
-        }
-
-        // Set intaking OFF
-        if (g2.wasJustPressed(GamepadKeys.Button.B)) {
-            if (!indexer.isBusy()) indexer.setIntaking(false);
-        }
-
-        indexer.update();
-
-        /*
-        // Toggle continuous lock
-        if (g2.wasJustPressed(GamepadKeys.Button.X)) {
-            continuousAprilTagLock = !continuousAprilTagLock;
-            if (continuousAprilTagLock) aprilTag.setCurrentCameraScannedId(0);
-        }
-
-        // Alliance selection
-        if (g2.wasJustPressed(GamepadKeys.Button.LEFT_BUMPER))
-            aprilTag.setGoalTagID(20); // blue
-        if (g2.wasJustPressed(GamepadKeys.Button.RIGHT_BUMPER))
-            aprilTag.setGoalTagID(24); // red
-        */
-
         // ========== TELEMETRY ==========
         telemetry.addData("Field Centric", fieldCentric);
-        //telemetry.addData("April Lock", continuousAprilTagLock);
-        //telemetry.addData("Turn Correction", turnCorrection);
         telemetry.addData("Indexer State", indexer.getState());
         telemetry.addData("Next State", indexer.nextState());
         telemetry.addData("Indexer Voltage", indexer.getVoltageAnalog());
-        telemetry.addData("Outtake Power", outtake.getPower());
+        telemetry.addData("Actuator up?: ",actuator.isActivated());
         telemetry.update();
     }
 }
