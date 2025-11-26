0ee02e7621c6b65dceef05987b5ab41107a0da0a
tango8 <ethan.b.tang@gmail.com>
2025-11-26 13:48:27 -0500

    apriltag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/AprilTag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/AprilTag.java
@@ -1,8 +1,6 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.vision.apriltag.AprilTagLibrary;
 import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
@@ -10,7 +8,6 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-
 import java.util.List;
 
 // TODO IMPORTANT NOTES: For goalTagID, just have separate teleops one for red alliance one for blue where blue teleop can setGoalTagID(20) and red teleop can setGoalTagID(24)
@@ -19,6 +16,7 @@ public class AprilTag {
     private int id;
     private int obeliskId;
     private int goalTagID; // our current alliance goal
+    private int cameraScannedId;
     private double bearing;
     private double elevation;
     private double range;
@@ -27,11 +25,13 @@ public class AprilTag {
 
     public AprilTag(HardwareMap hardwareMap) {
         AprilTagLibrary library = AprilTagGameDatabase.getCurrentGameTagLibrary();
+
         processor = new AprilTagProcessor.Builder()
                 .setTagLibrary(library)
                 .build();
+
         WebcamName webcamname = hardwareMap.get(WebcamName.class, "webcam");
-        portal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "webcam"), processor);
+        portal = VisionPortal.easyCreateWithDefaults(webcamname, processor);
     }
 
     public void toggle(boolean bool) {
@@ -51,28 +51,29 @@ public class AprilTag {
 
     public void scanGoalTag() {
         id = -1;
-        bearing = Integer.MAX_VALUE;
+        bearing = Double.NaN;
+        elevation = Double.NaN;
+        range = Double.NaN;
+
         // If camera is facing to the right of the center of the cam (if it needs to move to the left) the bearing is positive.
         List<AprilTagDetection> detectionList = processor.getDetections();
         for (AprilTagDetection detection : detectionList) {
+            cameraScannedId = detection.id;
             // goalTagID should be gotten before round/during auto
-            if (detection.id == goalTagID) {
-                range = detection.ftcPose.range; // distance from camera to center of tag
-                bearing = detection.ftcPose.bearing; // the angle (left/right) the camera must turn to directly point at the tag center
-                elevation = detection.ftcPose.elevation; // the angle (up/down) the camera must turn to directly point at the tag center
+            if (detection.id == goalTagID && detection.ftcPose != null) {
                 id = detection.id;
+                bearing = detection.ftcPose.bearing;
+                elevation = detection.ftcPose.elevation;
+                range = detection.ftcPose.range;
                 break;
             }
-        }
-
-        if (bearing == Integer.MAX_VALUE) {
-            bearing = 0;
         }
     }
 
     public void setGoalTagID(int allianceTagID) {
         goalTagID = allianceTagID;
     }
+    public int getCurrentId() { return cameraScannedId;}
     public int getObeliskId(){
         return obeliskId;
     }
@@ -84,5 +85,8 @@ public class AprilTag {
     }
     public double getBearing(){
         return bearing;
+    }
+    public void setCurrentCameraScannedId(int i) {
+        cameraScannedId = i;
     }
 }
