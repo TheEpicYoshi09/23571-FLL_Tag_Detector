3116edb0ba5f855dde67d7f1c54912b5f8f17245
Ryan Hill <rhill051@gmail.com>
2025-11-25 20:08:13 -0600

    renaming the vision to limelight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
@@ -13,27 +13,27 @@ public class Robot {
     //public IntakeSubsystem intake;
     public ShooterController shooter;
 
-    public LimeLightSubsystem vision;
+    public LimeLightSubsystem limelight;
 
     public Robot(HardwareMap hardwareMap, Telemetry telemetry) {
         // Initialize all subsystems in a controlled, predictable order
         drive = new DriveSubsystem(hardwareMap);
         shooter = new ShooterController(hardwareMap);
-        vision = new LimeLightSubsystem(hardwareMap);
+        limelight = new LimeLightSubsystem(hardwareMap);
         //intake = new IntakeSubsystem(hardwareMap);
     }
 
     public void addTelemetry(Telemetry telemetry) {
         drive.addTelemetry(telemetry);
         shooter.addTelemetry(telemetry);
-        vision.addTelemetry(telemetry);
+        limelight.addTelemetry(telemetry);
     }
 
     /** Called every control loop — optional, but great practice */
     public void periodic() {
         drive.periodic();
         shooter.update();
-        vision.periodic();
+        limelight.periodic();
         // Intake may not need periodic update
     }
 }
