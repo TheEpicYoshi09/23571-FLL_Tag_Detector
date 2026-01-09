632d7778d415eab650546810a5e03b71b0a5662b
ex-Potential <faris.ahmed.ilyas@gmail.com>
2026-01-09 18:17:13 -0500

    drsytdyu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/TeleOp/TeleOp1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/TeleOp/TeleOp1.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.TeleOp;
+package org.firstinspires.ftc.teamcode.decode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
