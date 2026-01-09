3a5fa5200e8fdc543d64470c5550dc5fa29e2430
Max <mthoma67@students.byron.k12.mn.us>
2026-01-09 14:34:11 -0600

    1/9/26 Worst comes to worst. This is what we use for competition.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/testAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/testAuto.java
@@ -17,27 +17,24 @@ public class testAuto extends LinearOpMode {
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
+        // --- Set up wheels ---
         robot.drive.resetEncoders();
         robot.drive.setRunToPositionMode();
-        robot.drive.setTargetForwardInches(-36, .8); // TODO: I'm not sure if a negative distance actually makes it go backwards.. it should right?
 
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()){
+        // --- Set up vision ---
+        // Set turret TX offset to +10 degrees → right
+        robot.turret.setTxOffset(10);
+
+        // Enable auto aim
+        robot.turret.toggleAutoAim();
+        robot.vision.clearAllowedTags();
+        robot.vision.addAllowedTag(24);
+
+        while (opModeIsActive()) {
             while (opModeIsActive()) {
-                robot.update(); // updates vision pose
 
-                robot.update(); // updates vision pose
-
-                if (robot.vision.hasFieldPose()) {
-                    double x = robot.vision.getFieldX();
-                    double y = robot.vision.getFieldY();
-                    double yaw = robot.vision.getFieldYaw();
-
-                    // auto drive logic here
-                }
             }
-            robot.shooter.update();
-            robot.update();
         }
     }
 }
+
