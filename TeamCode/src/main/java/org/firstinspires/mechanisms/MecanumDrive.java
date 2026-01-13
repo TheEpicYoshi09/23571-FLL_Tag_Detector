f17ff2df3d19a917548d909ac1e87a2c912e1732
tigerseyemax1 <maxmaxma26@gmail.com>
2026-01-13 17:08:22 -0500

    MecanumDrive v1

--- TeamCode/src/main/java/org/firstinspires/mechanisms/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/mechanisms/MecanumDrive.java
@@ -1,4 +1,42 @@
 package org.firstinspires.mechanisms;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
 public class MecanumDrive {
+    private DcMotor frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor;
+    private IMU imu;
+
+    public void init(HardwareMap hwMap) {
+        frontLeftMotor = hwMap.get(DcMotor.class, "frontLeft");
+        frontRightMotor = hwMap.get(DcMotor.class, "frontRight");
+        backLeftMotor = hwMap.get(DcMotor.class, "backLeft");
+        backRightMotor = hwMap.get(DcMotor.class, "backRight");
+
+        // Reverse half of the four motors
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // run with encoder to achieve a specific speed/velocity regardless of the friction
+        // motor uses PID controller in order to achieve the velocity
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        imu = hwMap.get(IMU.class, "imu");
+
+        RevHubOrientationOnRobot RevOrientation = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP;
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD);
+
+        imu.initialize(new IMU.Parameters(RevOrientation));
+    }
+
+    public void drive(double forward, double strafe, double rotate) {
+
+    }
 }
