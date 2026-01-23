f6f110343edc86a87273a0e1f29420bc8e6b494b
Frog <gsellinghaus@gmail.com>
2026-01-23 08:25:39 -0800

    asdf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rebuilt/Test_Programs/Gregory_Test_Files/ServoClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rebuilt/Test_Programs/Gregory_Test_Files/ServoClass.java
@@ -1,44 +1,73 @@
 package org.firstinspires.ftc.teamcode.Rebuilt.Test_Programs.Gregory_Test_Files;
 
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 /**
- * Represents an FTC servo with two preset positions.
+ * Represents an FTC servo or continuous rotation (CR) servo with two preset states.
  */
-class ServoPreset {
+class ServoClass {
+    public enum ServoType { STANDARD, CONTINUOUS_ROTATION }
+
+    private ServoType type;
     private String hardwareMapName;
     private double pos1;
     private double pos2;
-    private Servo servo; // The actual FTC Hardware Object
 
-    public ServoPreset(String name, double pos1, double pos2) {
+    // HardwareDevice is the common interface for ALL hardware
+    private HardwareDevice device;
+    private Servo standardServo;
+    private CRServo crServo;
+
+    public ServoClass(ServoType type, String name, double pos1, double pos2) {
+        this.type = type;
         this.hardwareMapName = name;
         this.pos1 = pos1;
         this.pos2 = pos2;
     }
 
-    /**
-     * Initializes the servo hardware from the hardwareMap.
-     */
     public void init(HardwareMap hwMap) {
-        servo = hwMap.get(Servo.class, hardwareMapName);
+        if (type == ServoType.STANDARD) {
+            standardServo = hwMap.get(Servo.class, hardwareMapName);
+            device = standardServo;
+        } else if (type == ServoType.CONTINUOUS_ROTATION) {
+            crServo = hwMap.get(CRServo.class, hardwareMapName);
+            device = crServo;
+        }
     }
 
-    // Command methods
+    /**
+     * Sets the position for a standard servo or the power for a CR servo.
+     *
+     * @param position The target position (for standard) or power (for CR).
+     */
+    public void goToPosition(double position) {
+        if (device == null) return;
+
+        if (type == ServoType.STANDARD) {
+            standardServo.setPosition(position);
+        } else {
+            // For a CR servo, the "position" parameter is treated as power.
+            crServo.setPower(position);
+        }
+    }
+
     public void goToPosition1() {
-        if (servo != null) servo.setPosition(pos1);
+        goToPosition(pos1);
     }
 
     public void goToPosition2() {
-        if (servo != null) servo.setPosition(pos2);
+        goToPosition(pos2);
     }
 
-    // Getters and Setters
     public double getPos1() { return pos1; }
     public void setPos1(double pos1) { this.pos1 = pos1; }
     public double getPos2() { return pos2; }
     public void setPos2(double pos2) { this.pos2 = pos2; }
     public String getName() { return hardwareMapName; }
-    public Servo getHardware() { return servo; }
+
+    // Returns the base HardwareDevice
+    public HardwareDevice getHardwareDevice() { return device; }
 }
